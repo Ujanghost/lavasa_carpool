dd31b33320e1e3de4d83821f03a10128fb7413fc
Ujan Galui <ujan.g570@gmail.com>
2023-11-20 10:56:40 +0530

    Update settings.py

--- lavasa_carpool/registration/settings.py
+++ lavasa_carpool/registration/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = 'django-insecure-leelgje@f1s=p1m@u&mixl3^zo!^*fa-4*&4&6evk*0@1+)kp9'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
