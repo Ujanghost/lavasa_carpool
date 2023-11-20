bc88e813360774c9a5bcae835cb5dfdfad4fd857
Ujan Galui <ujan.g570@gmail.com>
2023-11-20 10:15:29 +0530

    login system

--- lavasa_carpool/lcpl/urls.py
+++ lavasa_carpool/lcpl/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for lavasa_carpool project.
+"""registration URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from app1 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.SignupPage,name='signup'),
+    path('login/',views.LoginPage,name='login'),
+    path('home/',views.HomePage,name='home'),
+    path('logout/',views.LogoutPage,name='logout'),
+
+
+    
 ]
--- lavasa_carpool/manage.py
+++ lavasa_carpool/manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lavasa_carpool.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'registration.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
